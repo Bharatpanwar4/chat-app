96f3ef6e0d09509f3da8d473b002c630e5a3a926
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-15 01:22:54 +0530

    connected gmail fb to firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const config ={
+    apiKey: "AIzaSyBkPirhfwg0rTU6Yoib47oxXoIiDPeZEsM",
+    authDomain: "chat-web-app-4a844.firebaseapp.com",
+    projectId: "chat-web-app-4a844",
+    storageBucket: "chat-web-app-4a844.appspot.com",
+    messagingSenderId: "223471946903",
+    appId: "1:223471946903:web:0f7b15ae0fbabae672edd2"
+}
+
+
+const app=firebase.initializeApp(config);
