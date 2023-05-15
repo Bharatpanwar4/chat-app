9f058a4bbf97c3ac8113f91b98e25961d1ecc7ef
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-16 00:56:36 +0530

    created public and private route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React from 'react';
+// import { Switch } from 'react-router-dom';
+
 import 'rsuite/dist/rsuite.min.css';
-import 'rsuite/dist/rsuite-default.css'
+// import 'rsuite/dist/rsuite-default.css';
 import './styles/main.scss';
-function App() {
+import { BrowserRouter as Switch} from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+import SignIn from './pages/SignIn';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
+
+
+
+const App = () => {
   return (
-    <div>
-     hello
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
